8cafd2e7f6a47ea9ff5e6d54eca63d2cf876e73f
Mark Louis Bernardo <marklouisbernando318@gmail.com>
2025-01-15 22:03:17 +0800

    feat: added index page

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="js/script.js"></script>
+    <title>Mega Paragon</title>
+</head>
+<body>
+
+    <section class="w-full h-screen bg-slate-900">
+        <nav>
+            <!-- Navbar -->
+        </nav>
+
+        <!-- Hero Section -->
+        <div>
+            <!-- Code ni markme -->
+        </div>
+
+        <!-- Faqs -->
+        <div>
+            <!-- Faqs Redesign -->
+        </div>
+
+        <footer>
+            <!-- Footer -->
+        </footer>
+    </section>
+
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#151521',
+          secondary: '#ffd013ff',
+        }
+      }
+    }
+  }
