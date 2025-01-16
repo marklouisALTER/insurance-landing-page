b5ab8dd26dda17e9c400aa496feaa4b25e52019f
MarkmeDEV <markme.bernardo@gmail.com>
2025-01-16 16:13:56 +0800

    feat: added content

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <meta name="author" content="Mrkm">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Your financial advisor that will help you to your insurance.">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="js/script.js"></script>
     <title>Profile | Mega Paragon</title>
@@ -12,24 +16,49 @@
 </head>
 <body>
 
-    <section class="w-full h-screen bg-slate-900">
-        <nav>
-            <!-- Navbar -->
-        </nav>
-
+    <section class="w-full bg-slate-900 p-5">
         <!-- Hero Section -->
-        <div>
-            <!-- Code ni markme -->
+        <div class="w-full h-screen flex flex-col items-center justify-center  gap-2">
+            <div class="w-[15rem] h-[15rem] rounded-full border-4 border-secondary">
+
+            </div>
+            <div class="w-full max-w-[80vh] mt-5">
+                <div class="flex flex-col items-center mb-4 md:mb-7">
+                    <h3 class="font-primary md:text-xl text-white font-semibold">Welcome! I'm 👋</h3>
+                    <h1 class="text-secondary font-bold text-2xl md:text-3xl">Pauline Sanchez Moya</h1>
+                </div>
+                <h5 class="text-center font-primary text-sm md:text-lg text-gray-300 leading-loose">
+                A seasoned financial professional with over <strong class="text-secondary">2 decades of experience</strong> 
+                in helping individuals and businesses achieve their financial aspirations. My passion lies in 
+                elivering personalized financial solutions, from wealth management to retirement planning, 
+                ensuring every client feels confident about their financial future.
+                </h5>
+                <h5 class="text-center font-primary text-sm md:text-lg text-gray-300 mt-3 md:mt-5 leading-loose">
+                Through a comprehensive approach, I work closely with my clients to understand their unique goals, 
+                creating strategies that are both practical and effective. Let's work together to build a stable and 
+                prosperous financial journey.
+                </h5>
+                <div class="flex justify-center gap-5 mt-5 md:mt-10">
+                    <button class="bg-secondary text-primary font-medium font-primary py-2 px-5 rounded-md hover:bg-secondary/90 
+                        transition-all delay-100 ease-in-out">
+                        Join Mega Paragon
+                    </button>
+                    <button class="bg-secondary text-primary font-medium font-primary py-2 px-5 rounded-md hover:bg-secondary/90 
+                        transition-all delay-100 ease-in-out">
+                        Sign in as Pauline
+                    </button>
+
+                    <!-- <button>Sign in as Pauline</button> -->
+
+                </div>
+            </div>
+
         </div>
 
         <!-- Faqs -->
         <div>
             <!-- Faqs Redesign -->
         </div>
-
-        <footer>
-            <!-- Footer -->
-        </footer>
     </section>
 
 </body>
